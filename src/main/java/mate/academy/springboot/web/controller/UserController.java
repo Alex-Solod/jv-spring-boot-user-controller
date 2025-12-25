76cc67567405bfbf6719051c4d29d76803328f3f
Oleksandr Solodukhin <78688752+Alex-Solod@users.noreply.github.com>
2025-12-26 01:19:01 +0200

    fix: fix "User created. Id: %s, email: %s" message.

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
 
     @PostMapping
     private String save(@RequestBody User user) {
-        return "User created. Id: %s, email: "
+        return "User created. Id: %s, email: %s"
                 .formatted(user.id(), user.email());
     }
 }
